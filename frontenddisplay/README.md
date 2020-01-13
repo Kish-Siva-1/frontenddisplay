56986ea9631e944518b47a25d518d7640f2a3e9c
Kish-Siva-1 <rodes88@msn.com>
2020-01-13 02:41:23 +0100

    final commit

--- frontenddisplay/README.md
+++ frontenddisplay/README.md
@@ -1,68 +1,43 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# Front End Display
 
-## Available Scripts
+This is a website that demonstrates the use of some grid alignments using SASS.
 
-In the project directory, you can run:
+## Technology used
 
-### `yarn start`
+Built with:
 
-Runs the app in the development mode.<br />
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+* Javascript React Front-end
+* SASS for styling
 
-The page will reload if you make edits.<br />
-You will also see any lint errors in the console.
+## Features
 
-### `yarn test`
+* Provides a nested grid summary of pictures of holiday options
+* Includes highlight effects on hover
+* Responsive design optimised for Desktop (1024px by 650px) & iPhone 6/7/8 Plus (414px by 736px)
 
-Launches the test runner in the interactive watch mode.<br />
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+## Installation
 
-### `yarn build`
+To install this repository simply clone the repo (this one) by running:
 
-Builds the app for production to the `build` folder.<br />
-It correctly bundles React in production mode and optimizes the build for the best performance.
+```
+git clone git@github.com:Kish-Siva-1/frontenddisplay.git
+```
 
-The build is minified and the filenames include the hashes.<br />
-Your app is ready to be deployed!
+Open the file by running each of the commands below in a new terminal: 
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+1. `cd firesale-client/`
+2. `cd firesale-api/`
 
-### `yarn eject`
+Then install the relevant dependencies with:  
 
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
+* `npm install` (for JS)
+* `bundle install` (for Ruby on Rails)
 
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+Finally run both by using: 
 
-Instead, it will copy all the configuration files and the transitive dependencies (Webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
+* `npm start` (for JS)
+* `rails s` (for Ruby on Rails)
 
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
+## License
 
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
-
-### Analyzing the Bundle Size
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
-
-### Making a Progressive Web App
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
-### Advanced Configuration
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `yarn build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+MIT © Kish-Siva-1
--- frontenddisplay/src/App.js
+++ frontenddisplay/src/App.js
@@ -1,26 +1,110 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './App.scss';
+import logo from './images/logo.png'
+import rightarrow from './images/mobile/RightArrow.png'
+import leftarrow from './images/mobile/LeftArrow.png'
+import testcovimg from './images/mobile/TestimonialCoverImage.png'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      slideIndex: 0
+    };
+  }
+
+  componentDidMount() {
+    this.plusSlides(+1)
+  }
+
+  plusSlides = (n) => {
+    let m = this.state.slideIndex + n
+    var slides = document.getElementsByClassName('reisetopia-testimonials');
+
+    if (m > slides.length) { m = 1 }
+    else if (m < 1) { m = slides.length }
+
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+
+    slides[m - 1].style.display = "block";
+    this.setState({ slideIndex: m })
+  }
+
+  render() {
+
+    return (
+
+      <div className="App">
+      <div className="inner">
+
+        <section id='grid'>
+
+          <div className='wrapper'>
+            <div className='portrait'>
+              <img src="https://cdn.pixabay.com/photo/2014/09/13/12/28/hong-kong-443961_1280.jpg" alt="Hong Kong" />
+              <div className="center-bottom-portrait">Hong Kong <br /> ab 250€</div>
+            </div>
+            <div className='portrait'>
+              <img src="https://cdn.pixabay.com/photo/2015/01/01/15/34/berlin-585744_1280.jpg" alt="Berlin" />
+              <div className="center-bottom-portrait">Berlin <br /> ab 250€</div>
+            </div>
+            <div className='landscape'>
+              <img src="https://cdn.pixabay.com/photo/2014/08/01/15/51/manhattan-407703_1280.jpg" alt="New York" />
+              <div className="center-bottom-landscape">New York <br /> ab 250€</div>
+            </div>
+            <div className='landscape'>
+              <img src="https://cdn.pixabay.com/photo/2017/09/08/17/05/elephant-2729413_1280.jpg" alt="Ko Samui" />
+              <div className="center-bottom-landscape">Ko Samui <br /> ab 250€</div>
+            </div>
+          </div>
+
+        </section>
+
+        <section id='carousel'>
+
+          <div className="image-carousel">
+            <div className='coverimage'>
+              <img src={testcovimg} alt="Testimonial Cover" />
+            </div>
+            <div className='inner'>
+
+              <div className="reisetopia-testimonials">
+                <p className="quote">I had an incredible time at the Langmore Hotel in Melbourne, Australia.</p>
+                <p className="testimonials-author">John Keats</p>
+              </div>
+
+              <div className="reisetopia-testimonials">
+                <p className="quote">I thoroughly enjoyed my trip with Reisetopia and my booking was fast and very convenient.</p>
+                <p className="testimonials-author">Ernest Hemingway</p>
+              </div>
+
+              <div className="reisetopia-testimonials">
+                <p className="quote">I am incredibly happy with my purchase and the airline even upgraded my seat to first class!</p>
+                <p className="testimonials-author">Thomas A. Edison</p>
+              </div>
+
+            </div>
+            <div className="arrows">
+              <div className="prev" onClick={() => { this.plusSlides(-1) }} ><img src={leftarrow} alt="Left Arrow" /></div>
+              <div className="next" onClick={() => { this.plusSlides(+1) }} ><img src={rightarrow} alt="Right Arrow" /></div>
+            </div>
+          </div>
+
+        </section>
+
+        <section id='footer'>
+          <img src={logo} alt="reisetopia logo" />
+        </section>
+
+      </div>
+      </div>
+
+    );
+  }
 }
 
 export default App;
